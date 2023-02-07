036976997367f346dc689ff68ecf290c43da695e
pkjr002 <pkjr002@gmail.com>
2023-02-06 23:29:28 -0500

    :white_check_mark: FACTS is running :construction: NZcase, Match old NZ O/P :bug: ESL step requires NYC loc.

--- rr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -r re.session.amarel1.amarel.rutgers.edu.pk695.*
-
-rm -r /scratch/pk695/radical.pilot.sandbox/re.session.amarel1.amarel.rutgers.edu.pk695.019393.0003*
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# Activate the vene
+source ../ve3/bin/activate
+#
+#
+# Experiment
+EXP='NZrerun.OG.NZ.TG'
+EXP_FOLDER='experiments-pkjr002'
+#
+# RUN facts.
+python3 runFACTS.py $EXP_FOLDER/$EXP/ 2>&1 | tee $EXP_FOLDER/$EXP/OP-TERM-$EXP.txt
+
+
+
+
+
+# DO NOT USE Unless checked path
+##rm -r re.session.amarel1.amarel.rutgers.edu.pk695.*
+##rm -r /scratch/pk695/radical.pilot.sandbox/re.session.amarel1.amarel.rutgers.edu.pk695.019393.0003*
