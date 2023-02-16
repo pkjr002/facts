8cb2b04f2cbc90acdaf309c96a921b0c1e171eca
pkjr002 <pkjr002@gmail.com>
2023-02-16 00:48:24 -0500

    :memo: More dates are logged in the O/P file

--- run_main.sh
+++ run_main.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 # 
-start=$(date +%s)
+startOG=$(date +%s)
 # -------------------------------------------------------------------------------
-FULLTG=1
-total_lines=7435
-lines_per_iteration=500
+FULLTG=-1
+total_lines=-7435
+lines_per_iteration=-500
 source_file="latlon_basedON_2kmNZInsar_IPdata_.txt"
 target_file="location.lst"
 # -------------------------------------------------------------------------------
 # Activate the vene
-source ../ve3/bin/activate
+source ../ve3T/bin/activate
 #
 facts_dir=$(pwd)
 # FOLDERS
 EXP_FOLDER="experiments-pkjr002"
-EXP_MAIN="NZrerun"
-EXP=("NZrerun.ssp585.20k.50core")
+EXP_MAIN="FACTS_test"
+EXP=("onemodule")
 experiment_dir="$facts_dir/$EXP_FOLDER/$EXP_MAIN/$EXP"
 fileOUT="Tlog_$EXP_MAIN.$EXP.txt"      #"target_$((i/lines_per_iteration)).txt"
 # -------------------------------------------------------------------------------
 cd "$experiment_dir"
 #
 # Create a LOG file for the Experiment.
-echo -e "==> Started at: $(date --date=@$start)" > "$facts_dir/$fileOUT"
+echo -e "==> Started at: $(date --date=@$startOG)" > "$facts_dir/$fileOUT"
 echo -e "::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: \n" >> "$facts_dir/$fileOUT"
 #
 # Loop over the experiments (Have to make more efficient before using)
@@ -44,6 +44,8 @@ for exp in "${EXP[@]}"; do
       # Start logging the Experiment.   
       echo -e " ______________________________________________________________ " >> "$facts_dir/$fileOUT"
       echo -e "==> begin loop $i ... " >> "$facts_dir/$fileOUT"
+      start=$(date +%s)
+      echo "Started at:: $(date --date=@$start)" >> "$facts_dir/$fileOUT"
       echo -e "experiment NAME:: $exp  |:|  experiment folder:: $EXP_FOLDER" >> "$facts_dir/$fileOUT"
       echo -e "location.lst:: lines_to_copy=$lines_to_copy lines_copied=$lines_copied \n" >> "$facts_dir/$fileOUT"
       #
@@ -51,6 +53,8 @@ for exp in "${EXP[@]}"; do
       python3 runFACTS.py $EXP_FOLDER/$EXP_MAIN/$exp 2>&1 | tee -a $facts_dir/$fileOUT
       cd "$experiment_dir"
       #
+      end=$(date +%s)
+      echo "Ended at:: $(date --date=@$end)" >> "$facts_dir/$fileOUT"
       echo -e "\n ==> end \n" >> "$facts_dir/$fileOUT"
       # Rename output file to allow for next set of outputs. 
       output_set="output_set_$((i/lines_per_iteration))"
@@ -60,12 +64,16 @@ for exp in "${EXP[@]}"; do
   else # Only use If you have <500 locations.
     echo -e " ______________________________________________________________ " >> "$facts_dir/$fileOUT"
     echo -e "==> begin (single loop) ... " >> "$facts_dir/$fileOUT"
+    start=$(date +%s)
+    echo "Started at:: $(date --date=@$start)" >> "$facts_dir/$fileOUT"
     echo -e "experiment NAME:: $exp  |:|  experiment folder:: $EXP_FOLDER" >> "$facts_dir/$fileOUT"
     #
     cd "$facts_dir"
     python3 runFACTS.py $EXP_FOLDER/$EXP_MAIN/$exp 2>&1 | tee -a $facts_dir/$fileOUT
     cd $experiment_dir
     #
+    end=$(date +%s)
+    echo "Ended at:: $(date --date=@$end)" >> "$facts_dir/$fileOUT"
     echo -e "\n ==> end \n" >> "$facts_dir/$fileOUT"
   fi
 done
@@ -74,17 +82,18 @@ cd "$facts_dir"
 # Write the footer.
 echo -e "\n :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::" >> "$facts_dir/$fileOUT"
 echo "!! End of Program !!" >> "$facts_dir/$fileOUT"
-end=$(date +%s)
+endOG=$(date +%s)
 # Calculate the duration in seconds
-duration=$((end - start))
+duration=$((endOG - startOG))
 duration_min=$((duration / 60))
 # Print the start time, end time, and duration to a file
 echo -e "\nEXPERIMENT FOLDER:: "$EXP_FOLDER >> "$facts_dir/$fileOUT"
+echo -e "EXPERIMENT subFOLDER:: "$EXP >> "$facts_dir/$fileOUT"
 echo "EXPERIMENT:: "$exp >> "$facts_dir/$fileOUT"
 echo " ............... " >> "$facts_dir/$fileOUT"
-echo "Started at: $(date --date=@$start)" >> "$facts_dir/$fileOUT"
-echo "Ended at: $(date --date=@$end)" >> "$facts_dir/$fileOUT"
-echo "Duration: $duration_min minutes" >> "$facts_dir/$fileOUT"
+echo "Started at:: $(date --date=@$startOG)" >> "$facts_dir/$fileOUT"
+echo "Ended at:: $(date --date=@$endOG)" >> "$facts_dir/$fileOUT"
+echo "Duration:: $duration_min minutes" >> "$facts_dir/$fileOUT"
 
 
 
