bdc76b1f4f6e2397f5601e0c86e3d7249c4aefa8
pkjr002 <pkjr002@gmail.com>
2023-07-01 02:29:07 -0400

    :recycle: Exp Tests -coupling -esl

--- z_esl.ssp585.sh
+++ z_esl.ssp585.sh
@@ -13,7 +13,7 @@ source /scratch/pk695/FACTS/230525_FactsTest/$venv/bin/activate
 # .....................................................................................................................
 facts_dir=$(pwd)
 #
-EXP_FOLDER="experiments.TEST";       EXP_MAIN="coupling";        EXP="ssp585"
+EXP_FOLDER="experiments.TEST";       EXP_MAIN="esl_230630";        EXP="coupling.ssp585"
 echo "EXP_FOLDER=$EXP_FOLDER      EXP_MAIN=$EXP_MAIN      EXP=$EXP"
 #
 experiment_dir="$facts_dir/$EXP_FOLDER/$EXP_MAIN/$EXP"
