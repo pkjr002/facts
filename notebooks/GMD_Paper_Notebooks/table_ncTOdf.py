6ed1ed0c4a5139d01a21347279cda94d9c29e9ba
pkjr002 <pkjr002@gmail.com>
2023-07-14 13:33:18 -0400

    :construction:

--- notebooks/GMD_Paper_Notebooks/table_ncTOdf.py
+++ notebooks/GMD_Paper_Notebooks/table_ncTOdf.py
@@ -25,8 +25,14 @@ def module_Table_nc(df,SCENARIO,SCENARIO_R,EXPDATE,YEAR):
             #
             # Open the .nc data file. 
             if df.at[val,'dataID'] == 'cplng':
-                dataFOLDER  = '/projects/kopp/facts-experiments/{arg2}/coupling.{arg1}/output/'.format(arg1=scenario[1],arg2=EXPDATE)
-                dataFILE    = 'coupling.{arg1}.'.format(arg1=scenario[1]) + df.at[val,'DataFile']
+                # dataFOLDER  = '/projects/kopp/facts-experiments/{arg2}/coupling.{arg1}/output/'.format(arg1=scenario[1],arg2=EXPDATE)
+                # dataFILE    = 'coupling.{arg1}.'.format(arg1=scenario[1]) + df.at[val,'DataFile']
+                # dataFOLDER  = '/scratch/pk695/FACTS/002_fork/facts/experiments.TEST/coupling/{arg1}/output/'.format(arg1=scenario[1])
+                # dataFILE    = '{arg1}.'.format(arg1=scenario[1]) + df.at[val,'DataFile']
+                # alex 
+                dataFOLDER  = 'scratch/ar2208/branches/TotalingFix/facts/experiments/coupling.{arg1}/output'.format(arg1=scenario[1])
+                dataFILE    = '{arg1}.'.format(arg1=scenario[1]) + df["DataFile"][val]
+            #   
             elif df.at[val,'dataID'] == 'ar5k14':
                 dataFOLDER  = '/projects/kopp/facts-experiments/{arg2}/ar5k14.{arg1}/output/'.format(arg1=SCENARIO_R[scenario[0]],arg2=EXPDATE)
                 dataFILE    = 'ar5k14.{arg1}.'.format(arg1=SCENARIO_R[scenario[0]]) + df.at[val,'DataFile']
@@ -80,8 +86,14 @@ def wf_Table_nc(df,SCENARIO,EXPDATE,YEAR):
             if (scenario == 'ssp370') & ((workflow == 'wf3e') or (workflow == 'wf3f') or (workflow == 'wf4')):
                 continue
             # Open the .nc data file.
-            dataFOLDER  = '/projects/kopp/facts-experiments/{arg2}/coupling.{arg1}/output/'.format(arg1=scenario,arg2=EXPDATE)
-            dataFILE    = 'coupling.{arg1}.'.format(arg1=scenario) + df.at[val, 'DataFile']
+            # dataFOLDER  = '/projects/kopp/facts-experiments/{arg2}/coupling.{arg1}/output/'.format(arg1=scenario,arg2=EXPDATE)
+            # dataFILE    = 'coupling.{arg1}.'.format(arg1=scenario) + df.at[val, 'DataFile']
+            # dataFOLDER  = '/scratch/pk695/FACTS/002_fork/facts/experiments.TEST/coupling/{arg1}/output/'.format(arg1=scenario)
+            # dataFILE    = '{arg1}.'.format(arg1=scenario) + df.at[val,'DataFile']
+            # alex 
+            dataFOLDER  = 'scratch/ar2208/branches/TotalingFix/facts/experiments/coupling.{arg1}/output'.format(arg1=scenario)
+            dataFILE    = '{arg1}.'.format(arg1=scenario) + df["DataFile"][val]
+            #
             d_nc        = xr.open_dataset(dataFOLDER + dataFILE)
             #
             # Percentile calculation.
