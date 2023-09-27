9460c278fc84c125985855747a26c7f092bec792
pkjr002 <pkjr002@gmail.com>
2023-09-27 10:52:38 -0400

    :ambulance: ve_fix - force the ve created on the node to use the same as the one the pilot - after amarel 2309 update

--- FACTS.py
+++ FACTS.py
@@ -105,6 +105,7 @@ def GenerateTask(tcfg, ecfg, pipe_name, stage_name, task_name, workflow_name="",
 
     t.pre_exec = []     # Pre exec let you load modules, set environment before executing the workload
     # t.pre_exec.append("pip install psutil memory_profiler") #THIS PRE INSTALLS PSUTILS AND THE MEMORY PROFILER
+    t.pre_exec = ['. $RP_PILOT_SANDBOX/env/rp_named_env.rp.sh || true']
 
     if not "upload_input_data" in tcfg.keys():
         tcfg['upload_input_data'] = []
