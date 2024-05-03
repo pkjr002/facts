00bce1c511ef7706eba3a8ef4c1a0f3033a4eabb
pkjr002 <pkjr002@gmail.com>
2024-05-03 13:58:22 -0400

    Xplore FaIR

---
+++ notebooks/alternate_emissions_pathways/fun_altem.py
@@ -0,0 +1,18 @@
+import os
+    
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# FUNCTION BLOCK.
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+#
+def list_files_with_names(path, names):    
+    if not isinstance(names, list):
+        raise ValueError("The 'names' argument should be a list of strings.")
+    #
+    file_list = os.listdir(path)
+    file_list = sorted(file_list)
+    #
+    matching_files = []
+    for filename in file_list:
+        if all(name in filename for name in names):
+            matching_files.append(filename)
+    return matching_files
