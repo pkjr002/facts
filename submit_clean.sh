2c1dcd1904dea8930f283d9e549ae1eed66545b3
pkjr002 <pkjr002@gmail.com>
2025-11-04 19:19:18 -0500

    :wastebasket: script for rp clean - wipe rp sandbox contents and hidden files - del fronten rp

--- submit_clean.sh
+++ submit_clean.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-rm -rf /home/jovyan/radical.pilot.sandbox/re.session.*
+# Clean the radical sandbox (pilot/task). 
+rm -rf /home/jovyan/radical.pilot.sandbox/* \
+       /home/jovyan/radical.pilot.sandbox/.[!.]* \
+       /home/jovyan/radical.pilot.sandbox/..?*
 
+# Clean Frontend radical session folder
 rm -rf re.session.*
